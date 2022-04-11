6901500accd4fd4096bd0ec6714a1294e4d623ea
AlesiaZuk <78986532+AlesiaZuk@users.noreply.github.com>
2022-04-11 16:59:42 +0300

    Balance обновление

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,10 +1,16 @@
+import { useSelector } from 'react-redux';
+import { getBalance} from "../../redux/transactions/transactionsSelectors"
+
+
 import "../../css/main.min.css";
 
 function Balance() {
+  const balance = useSelector(getBalance);
+
   return (
     <div className="balanceWrap">
       <p className="balanceTitle">Ваш баланс</p>
-      <p className="balanceText">&#8372; 24000.00</p>
+      <p className="balanceText">&#8372; {balance}</p>
     </div>
   );
 }
