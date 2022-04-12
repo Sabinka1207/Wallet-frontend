5a609b5b24221c3e770787379415553cafdd7885
eche-unicorn <d.anasteysha@gmail.com>
2022-04-12 15:03:53 +0300

    added transaction

--- src/redux/statistics/statisticsOperation.js
+++ src/redux/statistics/statisticsOperation.js
@@ -1,19 +1,20 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 // import fetchStatistics from "../../service/transactions/statisticsApi";
 
 axios.defaults.baseURL = `https://pure-atoll-67904.herokuapp.com/api`;
 
 const getStatistics = createAsyncThunk(
-    'statistics/fetchStatistics', 
-    async(_, {rejectWithValue})=>{
-        try {
-                  axios.defaults.headers.common.Authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNGE5OTc4ZGY5OTNkODFmNTVlNjJlMSIsImlhdCI6MTY0OTcxMzE0NSwiZXhwIjoxNjQ5NzE2NzQ1fQ.bB4JBh3_fRxBu1DKFE-LB3X7mGDXzZDbOKZZUFGwu-Q`;
-            const statistics = await axios.get('/transactions/statistics');
-            return statistics.data;
-        } catch (error) {
-            return rejectWithValue(error)
-        }
-    })
+  'statistics/fetchStatistics',
+  async (_, { rejectWithValue }) => {
+    try {
+      //   axios.defaults.headers.common.Authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNGE5OTc4ZGY5OTNkODFmNTVlNjJlMSIsImlhdCI6MTY0OTcxMzE0NSwiZXhwIjoxNjQ5NzE2NzQ1fQ.bB4JBh3_fRxBu1DKFE-LB3X7mGDXzZDbOKZZUFGwu-Q`;
+      const statistics = await axios.get('/transactions/statistics');
+      return statistics.data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  },
+);
 
-    export default getStatistics;   
+export default getStatistics;
